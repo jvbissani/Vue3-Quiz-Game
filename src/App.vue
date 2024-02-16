b8db275179bd0e3b5beea1a13a447e4343f78077
jvbissani <joaobissani@gmail.com>
2024-02-16 12:15:15 -0300

    API Connection is working.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+
+    <template v-if="this.question">
+
+      <h1 v-html="this.question">
+      </h1>
+
+      <template v-for="(answer, index) in this.answers" :key="index">
+      <input
+      type="radio" 
+      name="options" 
+      value="answer">
+      
+      <label v-html="answer"></label><br>
+      </template>
+
+    </template>
+
+    <button class="send" type="button">Send</button>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+    return{
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+    }
+  },
+  computed: {
+    answers(){
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer); //Splice is used to change array elements based in their position.
+      return answers;
+    }
+  },
+  created(){
+    this.axios
+    .get('https://opentdb.com/api.php?amount=1')
+    .then((response) => {
+      this.question = response.data.results[0].question;
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      this.correctAnswer = response.data.results[0].correct_answer;
+    })  
   }
+
 }
+
 </script>
 
 <style lang="scss">
@@ -21,6 +63,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+  
+input[type=radio]{
+  margin: 12px 4px;
+}
+
+button.send{
+  margin-top: 12px;
+  height: 40px;
+  min-width: 120px;
+  padding: 0 16px;
+  color: #fff;
+  background-color: #1867c0;
+  border: 1px solid #06448a;
+  cursor: pointer;
+}
+
 }
 </style>
